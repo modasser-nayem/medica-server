f5891302923849155de72fa14283401d8ce6d9ee
modasser-nayem <modassernayem@gmail.com>
2026-07-24 15:33:21 +0600

    update the rate limit request size

--- src/app/middlewares/rateLimiter.ts
+++ src/app/middlewares/rateLimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const generalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // 100 requests per IP per window
+  max: 300, // 100 requests per IP per window
   standardHeaders: true,
   legacyHeaders: false,
   message: { message: "Too many requests, please try again later." },
@@ -10,7 +10,7 @@ export const generalLimiter = rateLimit({
 
 export const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 10, // only 10 attempts per 15 min per IP
+  max: 20, // only 10 attempts per 15 min per IP
   standardHeaders: true,
   legacyHeaders: false,
   message: { message: "Too many login attempts, please try again later." },
